16c3278defe1f48376ac5e76aacd748ddc1f8de0
ENZO-PC\ENZO <anthony.enzo.chang@gmail.com>
2024-07-12 22:08:23 +0800

    Add Developer Code Copyright Header

--- Project/app/main.py
+++ Project/app/main.py
@@ -1,1 +1,12 @@
-###
+'''***************************************************************
+# Copyright         : <Year> <Copyright Owner>
+* File Name         : <Filename>
+* Description       : <Write your file description here>
+*                    
+* Revision History  :
+* Date		Author 			Comments
+* ------------------------------------------------------------------
+* 07/11/2014	<Person working on the file>	<Short change description>
+*
+****************************************************************'''
+
